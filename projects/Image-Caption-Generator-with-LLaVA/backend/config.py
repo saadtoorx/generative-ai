db0da22738249bf21a9884e3a508f8f85100651f
Saad Toor <143895872+saadtoorx@users.noreply.github.com>
2026-01-28 12:19:03 +0500

    Delete projects/Image-Caption-Generator-with-LLaVA directory

--- projects/Image-Caption-Generator-with-LLaVA/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Ollama Settings
-OLLAMA_API_URL = os.getenv("OLLAMA_API_URL", "http://localhost:11434/api/generate")
-MODEL_NAME = os.getenv("MODEL_NAME", "llava")
-
-# Server Settings
-HOST = os.getenv("HOST", "127.0.0.1")
-PORT = int(os.getenv("PORT", 8000))
